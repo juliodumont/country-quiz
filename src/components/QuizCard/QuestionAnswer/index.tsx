69149bf16ce30c61c4432175982ee8c61340145a
juliodumont <julioef.dumont@gmail.com>
2022-08-05 20:16:58 -0300

    Layout now responsive

--- src/components/QuizCard/QuestionAnswer/index.tsx
+++ src/components/QuizCard/QuestionAnswer/index.tsx
@@ -9,19 +9,23 @@ type QuestionAnswerType = {
 
 const handleClick = () => {
   return null;
-}
+};
 
 export function QuestionAnswer(props: QuestionAnswerType) {
-  const hasAnswered = ['true', 'false', 'disabled'].includes(props.setAnswerState)
+  const hasAnswered = ["true", "false", "disabled"].includes(
+    props.setAnswerState
+  );
   return (
-    <button className={`question-container answer-${props.setAnswerState}`} onClick={!hasAnswered ? props.onClick : handleClick}>
+    <button
+      className={`question-container answer-${props.setAnswerState}`}
+      onClick={!hasAnswered ? props.onClick : handleClick}
+    >
+      <h3 className="question-number">{props.questionNumber}</h3>
       <p>
-        <span className="question-number">
-          {props.questionNumber}
-        </span>
         {props.questionAnswer}
-        {(props.setAnswerState === 'false' ||   props.setAnswerState === 'true') &&
-          (props.setAnswerState === 'false' ? (
+        {(props.setAnswerState === "false" ||
+          props.setAnswerState === "true") &&
+          (props.setAnswerState === "false" ? (
             <span className="material-icons answer-icon">cancel</span>
           ) : (
             <span className="material-icons answer-icon">check_circle</span>
